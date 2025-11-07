2e6f8cc42b2e74e4ca173abe98b018fa548d339d
izar06 <izar.anam22@gmail.com>
2025-11-07 09:21:02 +0700

    Add Cart Per Item

--- test/test_sauce_demo.py
+++ test/test_sauce_demo.py
@@ -5,167 +5,233 @@ from data.data import Data
 from pages.detail_items import Detail
 import pytest
 
-# def test_login_positive(setup):
-#     login = Login(setup)
-#     dashboard = Dashboard(setup)
-#     login.input_username("standard_user")
-#     login.input_password("secret_sauce")
-#     login.click_login_button()
-#     validation_text = dashboard.validation_dashboard()
-#     print(validation_text)
-#     assert validation_text == "Swag Labs"
+def test_login_positive(setup):
+    login = Login(setup)
+    dashboard = Dashboard(setup)
+    login.input_username("standard_user")
+    login.input_password("secret_sauce")
+    login.click_login_button()
+    validation_text = dashboard.validation_dashboard()
+    print(validation_text)
+    assert validation_text == "Swag Labs"
 
-# def test_login_field_username_kosong(setup):
-#     login = Login(setup)
-#     login.input_username("")
-#     login.input_password("secret_sauce")
-#     login.click_login_button()
-#     validation_text = login.validation_error_required_username()
-#     print(validation_text)
-#     assert validation_text == "Epic sadface: Username is required"
+def test_login_field_username_kosong(setup):
+    login = Login(setup)
+    login.input_username("")
+    login.input_password("secret_sauce")
+    login.click_login_button()
+    validation_text = login.validation_error_required_username()
+    print(validation_text)
+    assert validation_text == "Epic sadface: Username is required"
 
-# def test_login_field_password_kosong(setup):
-#     login = Login(setup)
-#     login.input_username("standard_user")
-#     login.input_password("")
-#     login.click_login_button()
-#     validation_text = login.validation_error_required_password()
-#     print(validation_text)
-#     assert validation_text == "Epic sadface: Password is required"
+def test_login_field_password_kosong(setup):
+    login = Login(setup)
+    login.input_username("standard_user")
+    login.input_password("")
+    login.click_login_button()
+    validation_text = login.validation_error_required_password()
+    print(validation_text)
+    assert validation_text == "Epic sadface: Password is required"
 
-# test_data_negative = Data.test_data_negative
+test_data_negative = Data.test_data_negative
 
-# @pytest.mark.parametrize("username, password, expected_error", test_data_negative)
-# def test_login_negative(setup, username, password, expected_error):
-#     login = Login(setup)
-#     login.input_username(username)
-#     login.input_password(password)
-#     login.click_login_button()
-#     validation_text = login.validation_error_wrong_username_and_password()
-#     print(validation_text)
-#     assert validation_text == expected_error
+@pytest.mark.parametrize("username, password, expected_error", test_data_negative)
+def test_login_negative(setup, username, password, expected_error):
+    login = Login(setup)
+    login.input_username(username)
+    login.input_password(password)
+    login.click_login_button()
+    validation_text = login.validation_error_wrong_username_and_password()
+    print(validation_text)
+    assert validation_text == expected_error
 
-# def test_show_detail_sauce_labs_backpack(setup):
-#     login = Login(setup)
-#     dashboard = Dashboard(setup)
-#     login.input_username("standard_user")
-#     login.input_password("secret_sauce")
-#     login.click_login_button()
-#     dashboard.show_detail_sauce_labs_backpack()
-#     validation_text = (dashboard.validation_detail_items())
-#     print(validation_text)
-#     assert validation_text in ("Back to products", "Sauce Labs Backpack")
+def test_show_detail_sauce_labs_backpack(setup):
+    login = Login(setup)
+    dashboard = Dashboard(setup)
+    login.input_username("standard_user")
+    login.input_password("secret_sauce")
+    login.click_login_button()
+    dashboard.show_detail_sauce_labs_backpack()
+    validation_text = (dashboard.validation_detail_items())
+    print(validation_text)
+    assert validation_text in ("Back to products", "Sauce Labs Backpack")
 
-# def test_show_detail_sauce_labs_bike_light(setup):
-#     login = Login(setup)
-#     dashboard = Dashboard(setup)
-#     login.input_username("standard_user")
-#     login.input_password("secret_sauce")
-#     login.click_login_button()
-#     dashboard.show_detail_sauce_labs_bike_light()
-#     validation_text = dashboard.validation_detail_items()
-#     print(validation_text)
-#     assert validation_text == "Back to products"
+def test_show_detail_sauce_labs_bike_light(setup):
+    login = Login(setup)
+    dashboard = Dashboard(setup)
+    login.input_username("standard_user")
+    login.input_password("secret_sauce")
+    login.click_login_button()
+    dashboard.show_detail_sauce_labs_bike_light()
+    validation_text = dashboard.validation_detail_items()
+    print(validation_text)
+    assert validation_text == "Back to products"
 
-# def test_show_detail_sauce_labs_bolt_tshirt(setup):
-#     login = Login(setup)
-#     dashboard = Dashboard(setup)
-#     login.input_username("standard_user")
-#     login.input_password("secret_sauce")
-#     login.click_login_button()
-#     dashboard.show_detail_sauce_labs_bolt_tshirt()
-#     validation_text = dashboard.validation_detail_items()
-#     print(validation_text)
-#     assert validation_text == "Back to products"
+def test_show_detail_sauce_labs_bolt_tshirt(setup):
+    login = Login(setup)
+    dashboard = Dashboard(setup)
+    login.input_username("standard_user")
+    login.input_password("secret_sauce")
+    login.click_login_button()
+    dashboard.show_detail_sauce_labs_bolt_tshirt()
+    validation_text = dashboard.validation_detail_items()
+    print(validation_text)
+    assert validation_text == "Back to products"
 
-# def test_show_detail_sauce_labs_fleece_jacket(setup):
-#     login = Login(setup)
-#     dashboard = Dashboard(setup)
-#     login.input_username("standard_user")
-#     login.input_password("secret_sauce")
-#     login.click_login_button()
-#     dashboard.show_detail_sauce_labs_fleece_jacket()
-#     validation_text = dashboard.validation_detail_items()
-#     print(validation_text)
-#     assert validation_text == "Back to products"
+def test_show_detail_sauce_labs_fleece_jacket(setup):
+    login = Login(setup)
+    dashboard = Dashboard(setup)
+    login.input_username("standard_user")
+    login.input_password("secret_sauce")
+    login.click_login_button()
+    dashboard.show_detail_sauce_labs_fleece_jacket()
+    validation_text = dashboard.validation_detail_items()
+    print(validation_text)
+    assert validation_text == "Back to products"
 
-# def test_show_detail_sauce_labs_onesia(setup):
-#     login = Login(setup)
-#     dashboard = Dashboard(setup)
-#     login.input_username("standard_user")
-#     login.input_password("secret_sauce")
-#     login.click_login_button()
-#     dashboard.show_detail_sauce_labs_onesia()
-#     validation_text = dashboard.validation_detail_items()
-#     print(validation_text)
-#     assert validation_text == "Back to products"
+def test_show_detail_sauce_labs_onesia(setup):
+    login = Login(setup)
+    dashboard = Dashboard(setup)
+    login.input_username("standard_user")
+    login.input_password("secret_sauce")
+    login.click_login_button()
+    dashboard.show_detail_sauce_labs_onesia()
+    validation_text = dashboard.validation_detail_items()
+    print(validation_text)
+    assert validation_text == "Back to products"
 
-# def test_show_detail_tshirt_red(setup):
-#     login = Login(setup)
-#     dashboard = Dashboard(setup)
-#     login.input_username("standard_user")
-#     login.input_password("secret_sauce")
-#     login.click_login_button()
-#     dashboard.show_detail_tshirt_red()
-#     validation_text = dashboard.validation_detail_items()
-#     print(validation_text)
-#     assert validation_text == "Back to products"
+def test_show_detail_tshirt_red(setup):
+    login = Login(setup)
+    dashboard = Dashboard(setup)
+    login.input_username("standard_user")
+    login.input_password("secret_sauce")
+    login.click_login_button()
+    dashboard.show_detail_tshirt_red()
+    validation_text = dashboard.validation_detail_items()
+    print(validation_text)
+    assert validation_text == "Back to products"
 
-# def test_show_detail_tshirt_red(setup):
-#     login = Login(setup)
-#     dashboard = Dashboard(setup)
-#     login.input_username("standard_user")
-#     login.input_password("secret_sauce")
-#     login.click_login_button()
-#     dashboard.show_detail_tshirt_red()
-#     validation_text = dashboard.validation_detail_items()
-#     print(validation_text)
-#     assert validation_text in ("Back to products")
+def test_show_detail_tshirt_red(setup):
+    login = Login(setup)
+    dashboard = Dashboard(setup)
+    login.input_username("standard_user")
+    login.input_password("secret_sauce")
+    login.click_login_button()
+    dashboard.show_detail_tshirt_red()
+    validation_text = dashboard.validation_detail_items()
+    print(validation_text)
+    assert validation_text in ("Back to products")
 
-# def test_add_to_cart(setup):
-#     login = Login(setup)
-#     dashboard = Dashboard(setup)
-#     detail = Detail(setup)
-#     login.input_username("standard_user")
-#     login.input_password("secret_sauce")
-#     login.click_login_button()
-#     dashboard.show_detail_tshirt_red()
-#     detail.add_to_cart()
-#     validation = detail.validation()
-#     print(validation)
-#     assert "1" in validation
-
-# def test_remove_cart(setup):
-#     login = Login(setup)
-#     dashboard = Dashboard(setup)
-#     detail = Detail(setup)
-#     login.input_username("standard_user")
-#     login.input_password("secret_sauce")
-#     login.click_login_button()
-#     dashboard.show_detail_tshirt_red()
-#     detail.add_to_cart()
-#     detail.remove_item_cart()
-#     validation = detail.validation()
-#     print(validation)
-#     assert "1" not in validation
-    
 def test_add_to_cart(setup):
     login = Login(setup)
     dashboard = Dashboard(setup)
+    detail = Detail(setup)
+    login.input_username("standard_user")
+    login.input_password("secret_sauce")
+    login.click_login_button()
+    dashboard.show_detail_tshirt_red()
+    detail.add_to_cart()
+    validation = detail.validation()
+    print(validation)
+    assert "1" in validation
+
+def test_remove_cart(setup):
+    login = Login(setup)
+    dashboard = Dashboard(setup)
+    detail = Detail(setup)
+    login.input_username("standard_user")
+    login.input_password("secret_sauce")
+    login.click_login_button()
+    dashboard.show_detail_tshirt_red()
+    detail.add_to_cart()
+    detail.remove_item_cart()
+    validation = detail.validation()
+    print(validation)
+    assert "1" not in validation
+    
+def test_add_to_cart_all_item(setup):
+    login = Login(setup)
+    dashboard = Dashboard(setup)
     cart = Cart(setup)
-    # detail = Detail(setup)
+    detail = Detail(setup)
     login.input_username("standard_user")
     login.input_password("secret_sauce")
     login.click_login_button()
     dashboard.add_all_product()
     dashboard.click_cart()
+    
+    validation = detail.validation()
+    print(validation)
+    assert "6" in validation
+    
     title = cart.get_name_title()
     print(title)
-    # validation = dashboard.validation_cart()
-    
     assert ['Sauce Labs Backpack', 'Sauce Labs Bike Light', 'Sauce Labs Bolt T-Shirt', 'Sauce Labs Fleece Jacket', 'Sauce Labs Onesie', 'Test.allTheThings() T-Shirt (Red)'] == title
-    # detail.add_to_cart()
-    # validation = detail.validation()
-    # print(validation)
-    # assert "1" in validation
+    
+
+def test_add_to_cart_item_Sauce_Labs_Backpack(setup):
+    login = Login(setup)
+    dashboard = Dashboard(setup)
+    login.input_username("standard_user")
+    login.input_password("secret_sauce")
+    login.click_login_button()
+    dashboard.add_to_cart_Sauce_Labs_Backpack()
+    
+    validation = dashboard.validation_cart()
+    assert "1" in validation
+
+def test_add_to_cart_item_Sauce_Labs_Bike_Light(setup):
+    login = Login(setup)
+    dashboard = Dashboard(setup)
+    login.input_username("standard_user")
+    login.input_password("secret_sauce")
+    login.click_login_button()
+    dashboard.add_to_cart_Sauce_Labs_Bike_Light()
+    
+    validation = dashboard.validation_cart()
+    assert "1" in validation
+    
+def test_add_to_cart_item_Sauce_Labs_Bolt_TShirt(setup):
+    login = Login(setup)
+    dashboard = Dashboard(setup)
+    login.input_username("standard_user")
+    login.input_password("secret_sauce")
+    login.click_login_button()
+    dashboard.add_to_cart_Sauce_Labs_Bolt_TShirt()
+    
+    validation = dashboard.validation_cart()
+    assert "1" in validation
+
+def test_add_to_cart_item_Sauce_Labs_Fleece_Jacket(setup):
+    login = Login(setup)
+    dashboard = Dashboard(setup)
+    login.input_username("standard_user")
+    login.input_password("secret_sauce")
+    login.click_login_button()
+    dashboard.add_to_cart_Sauce_Labs_Fleece_Jacket()
+    
+    validation = dashboard.validation_cart()
+    assert "1" in validation
+
+def test_add_to_cart_item_Sauce_Labs_Onesie(setup):
+    login = Login(setup)
+    dashboard = Dashboard(setup)
+    login.input_username("standard_user")
+    login.input_password("secret_sauce")
+    login.click_login_button()
+    dashboard.add_to_cart_Sauce_Labs_Onesie()
+    
+    validation = dashboard.validation_cart()
+    assert "1" in validation
+
+def test_add_to_cart_item_Test_allTheThings(setup):
+    login = Login(setup)
+    dashboard = Dashboard(setup)
+    login.input_username("standard_user")
+    login.input_password("secret_sauce")
+    login.click_login_button()
+    dashboard.add_to_cart_Test_allTheThings_TShirt()
+    
+    validation = dashboard.validation_cart()
+    assert "1" in validation
     
